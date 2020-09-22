2959f2fd57bb5a2dad4e97a1e22a676b51155440
Vassilyev Denis <tarapiygin@gmail.com>
2020-09-22 10:39:26 +0300

    added e2e tests

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,59 +1,65 @@
-// import puppetteer from 'puppeteer';
+import puppetteer from 'puppeteer';
 
-// const childProcess = require('child_process');
+const childProcess = require('child_process');
 
-// let server = null;
+let server = null;
 
-// jest.setTimeout(30000); // default puppeteer timeout
-// describe('popover', () => {
-//   let browser = null;
-//   let page = null;
-//   const baseUrl = 'http://localhost:9000';
-//   beforeAll(async () => {
-//     server = await childProcess.fork(`${__dirname}/test-server.js`);
-//     await new Promise((resolve, reject) => {
-//       server.on('error', () => {
-//         reject();
-//       });
-//       server.on('message', (message) => {
-//         if (message === 'ok') {
-//           resolve();
-//         }
-//       });
-//     });
+jest.setTimeout(30000); // default puppeteer timeout
+describe('test', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:9000';
+  beforeAll(async () => {
+    server = await childProcess.fork(`${__dirname}/test-server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', () => {
+        reject();
+      });
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
 
-//     browser = await puppetteer.launch({
-//       // Опции в методе launch нужно закомментировать при запуске в CI.
-//       // headless: false, // show gui
-//       // slowMo: 500,
-//       // devtools: true, // show devTools
-//     });
-//     page = await browser.newPage();
-//   });
-//   afterAll(async () => {
-//     await browser.close();
-//     server.kill();
-//   });
-//   // test code here
-//   // test start
-//   describe('Checking the appearance popover', () => {
-//     test('should add .popover_visible class when clicking on the button', async () => {
-//       await page.goto(baseUrl);
-//       const button = await page.$('.btn');
-//       button.click();
-//       // expect(await form.$('#entercardState').innerText).toBe('проверено');
-//       await page.waitForSelector('.popover.popover_visible');
-//     });
-//   });
-//   describe('Checking if the popover is hidden', () => {
-//     test('should remove the .popover_visible class on button click', async () => {
-//       await page.goto(baseUrl);
-//       const button = await page.$('.btn');
-//       button.click();
-//       button.click();
-//       // expect(await form.$('#entercardState').innerText).toBe('проверено');
-//       await page.waitForSelector('.popover');
-//     });
-//   });
-//   // test end
-// });
+    browser = await puppetteer.launch({
+      // Опции в методе launch нужно закомментировать при запуске в CI.
+      // headless: false, // show gui
+      // slowMo: 1000,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+  // test code here
+  // test start
+  describe('Checking for a popup form to appear', () => {
+    test('should open a form to create a product', async () => {
+      await page.goto(baseUrl);
+      const button = await page.$('.create-button');
+      button.click();
+      // expect(await form.$('#entercardState').innerText).toBe('проверено');
+      await page.waitForSelector('.pop-up.pop-up_visible');
+    });
+  });
+
+  describe('Product creation check', () => {
+    test('A product with a "Test" name and a price of 100 should be created', async () => {
+      await page.goto(baseUrl);
+      const button = await page.$('.create-button');
+      button.click();
+      const form = await page.$('#product-form');
+      const nameInput = await form.$('#product-form_name');
+      const priceInput = await form.$('#product-form_price');
+      const submitButton = await form.$('.save-button');
+      await nameInput.type('Test');
+      await priceInput.type('100');
+      submitButton.click();
+      await page.waitForSelector('tr[data-name="Test"]');
+    });
+  });
+  // test end
+});
